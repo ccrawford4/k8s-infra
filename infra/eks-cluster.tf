15c01f67b61e41660d55157701dba60c06c59d50
ccrawford4 <calumcrawford9@gmail.com>
2025-04-27 21:12:04 -0700

    working web on eks

--- infra/eks-cluster.tf
+++ infra/eks-cluster.tf
@@ -6,6 +6,11 @@ module "eks" {
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
+  enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access           = true
+  cluster_endpoint_private_access          = true
+
+
   eks_managed_node_groups = {
     worker-group-1 = {
       instance_types                = ["t2.small"]
