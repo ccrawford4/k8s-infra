86aa8d4ef61b1a4e1aec56298ecf89fe647bf55c
ccrawford4 <calumcrawford9@gmail.com>
2025-05-03 15:57:01 -0700

    rds + promote workflow

--- infra/variables.tf
+++ infra/variables.tf
@@ -13,3 +13,19 @@ variable "profile" {
 variable "iam_role_arn" {
   type = string
 }
+
+variable "environment" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
+variable "db_port_number" {
+  type = string
+}
